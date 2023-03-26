b4d6cb452b47db71c7c6150162e116880d1f8bf7
ParmbeerJohal <parmbeerjohal@gmail.com>
2023-03-26 00:36:13 -0700

    path updates, refactor functions

--- web-app-tracker-backend/src/server.js
+++ web-app-tracker-backend/src/server.js
@@ -9,39 +9,16 @@ var data;
 // Read data from JSON file
 fs.readFile("./data.json", "utf8", (err, jsonString) => {
     if (err) {
-    console.log("File read failed:", err);
-    return;
+        console.log("File read failed:", err);
+        return;
     }
 
     data = JSON.parse(jsonString);
 });
 
-// Get a single record
-function getWebAppRecord(productId) {
-    var dataArray = data;
-    const record = dataArray.find((record) => record.productId === productId);
-    if (record) {
-        return record;
-    }
-
-    return null;
-}
-
-// Update a single record
-function updateWebAppRecord(webAppProject, productId) {
-    var dataArray = data;
-    const recordIndex = dataArray.findIndex((record) => record.productId === productId);
-    if (recordIndex >= 0) {
-        dataArray[recordIndex] = webAppProject;
-        return webAppProject;
-    }
-
-    return null;
-}
-
 // Get all records
-app.get("/data", (req, res) => {
-    if(data) {
+app.get("/api/webapps", (req, res) => {
+    if (data) {
         res.send(data);
     } else {
         res.sendStatus(404);
@@ -49,10 +26,11 @@ app.get("/data", (req, res) => {
 });
 
 // Get a single record
-app.get("/data/:productId", (req, res) => {
+app.get("/api/webapps/:productId", (req, res) => {
     const productId = req.params.productId;
-    var record = getWebAppRecord(productId);
-    if(record) {
+    const record = data.find((record) => record.productId === productId);
+
+    if (record) {
         res.send(record);
     } else {
         res.sendStatus(404);
@@ -60,11 +38,13 @@ app.get("/data/:productId", (req, res) => {
 });
 
 // Update a single record
-app.put("/data/:productId/update", (req, res) => {
+app.put("/api/webapps/:productId/update", (req, res) => {
     const productId = req.params.productId;
     const webAppProject = req.body;
-    var record = updateWebAppRecord(webAppProject, productId);
-    if(record) {
+    const recordIndex = data.findIndex((record) => record.productId === productId);
+
+    if (recordIndex >= 0) {
+        data[recordIndex] = webAppProject;
         res.sendStatus(200);
     } else {
         res.sendStatus(404);
@@ -72,16 +52,17 @@ app.put("/data/:productId/update", (req, res) => {
 });
 
 // Add a new record
-app.post("/addwebapp", (req, res) => {
+app.post("/api/webapps/addwebapp", (req, res) => {
     const webAppProject = req.body;
     data.push(webAppProject);
     res.sendStatus(200);
 });
 
 // Delete a record
-app.delete("/data/:productId/delete", (req, res) => {
+app.delete("/api/webapps/:productId/deletewebapp", (req, res) => {
     const productId = req.params.productId;
     const recordIndex = data.findIndex((record) => record.productId === productId);
+
     if (recordIndex >= 0) {
         data.splice(recordIndex, 1);
         res.sendStatus(200);
